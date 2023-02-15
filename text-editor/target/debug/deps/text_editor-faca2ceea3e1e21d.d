e32449a2e7c186ce5597fc16c1be2068f4e897bc
carolinechen99 <carolinexyc99@gmail.com>
2023-02-14 22:30:02 -0500

    amend rock make invoke

---
+++ text-editor/target/debug/deps/text_editor-faca2ceea3e1e21d.d
@@ -0,0 +1,5 @@
+/Users/carolinechen/rust-miniprojects/text-editor/target/debug/deps/text_editor-faca2ceea3e1e21d: src/main.rs
+
+/Users/carolinechen/rust-miniprojects/text-editor/target/debug/deps/text_editor-faca2ceea3e1e21d.d: src/main.rs
+
+src/main.rs:
---
+++ text-editor/target/debug/text-editor.d
@@ -0,0 +1,1 @@
+/Users/carolinechen/rust-miniprojects/text-editor/target/debug/text-editor: /Users/carolinechen/rust-miniprojects/text-editor/src/main.rs
