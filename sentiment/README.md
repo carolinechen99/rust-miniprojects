7fb14e14ea615f807abfb4ea5e7333b34345f8f7
carolinechen99 <carolinexyc99@gmail.com>
2023-04-19 20:41:49 -0400

    final

---
+++ sentiment/README.md
@@ -0,0 +1,25 @@
+# Dict
+
+## Overview
+
+## Usage
+
+1. cd into the sentiment directory `sentiment`
+2. To run the program, type `cargo run -- analyze --sentence <sentence>`. The program will return the sentiment of the sentence.
+
+   For example, to analyze the sentence "I love Rust", type:
+
+   ```bash
+   cargo run -- analyze --sentence "I love Rust"
+   ```
+
+   And the program will return:
+
+   ```bash
+    Sentiment score: 3
+    comparative score: 1
+    Positive words: ["love"]
+    Negative words: []
+   ```
+
+3. Use the command `cargo run -- analyze --help` for a description of how to run the program
--- sentiment/src/main.rs
+++ sentiment/src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use sentiment::analyze;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(
+    version = "1.0",
+    author = "Caroline Chen",
+    about = "CLI for Sentiment Analysis"
+)]
+
+struct Cli {
+    #[clap(subcommand)]
+    command: Option<Commands>,
 }
+
+#[derive(Parser)]
+enum Commands {
+    #[clap(about = "Sentence to analyze")]
+    Analyze {
+        #[clap(short, long)]
+        sentence: String,
+    },
+}
+
+fn main() {
+    let args = Cli::parse();
+    match args.command {
+        Some(Commands::Analyze { sentence }) => {
+            let result = analyze(sentence);
+            println!("Sentiment score: {}", result.score);
+            println!("comparative score: {}", result.comparative);
+            println!("Positive words: {:?}", result.positive.words);
+            println!("Negative words: {:?}", result.negative.words);
+        }
+        None => {
+            println!("No command given");
+        }
+    }
+}
+
