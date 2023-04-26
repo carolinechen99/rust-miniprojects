d9926326e6a6aefdc79d64cea872385dbc858afe
carolinechen99 <carolinexyc99@gmail.com>
2023-04-26 15:52:35 -0400

    final

---
+++ what-to-wear/README.md
@@ -0,0 +1,19 @@
+# What to Wear
+
+## Overview
+
+This is a simple project that gives out a suggestion of what to wear based on randomness.
+
+## Usage
+
+type in the following command in the terminal:
+
+```bash
+cargo run
+```
+
+You can get a suggestion of what to wear for the day. For example:
+
+```bash
+Today's outfit: sandals with a t-shirt and shorts.
+```
--- what-to-wear/src/main.rs
+++ what-to-wear/src/main.rs
@@ -1,3 +1,39 @@
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
 fn main() {
-    println!("Hello, world!");
+    let shoes = [
+        "sneakers",
+        "boots",
+        "sandals",
+        "loafers",
+    ];
+
+    let tops = [
+        "t-shirt",
+        "sweater",
+        "blouse",
+        "button-down shirt",
+    ];
+
+    let bottoms = [
+        "jeans",
+        "shorts",
+        "skirt",
+        "trousers",
+    ];
+
+    let mut rng = thread_rng();
+
+    let selected_shoe = shoes.choose(&mut rng).unwrap();
+    let selected_top = tops.choose(&mut rng).unwrap();
+    let selected_bottom = bottoms.choose(&mut rng).unwrap();
+
+    println!(
+        "Today's outfit: {} with a {} and {}.",
+        selected_shoe, selected_top, selected_bottom
+    );
+    // format!(
+    //     "Today's outfit: {selected_shoe} with a {selected_top} and {selected_bottom}." 
+    // );
 }
